460dfe6901487436a40001808a79915837b62bec
Brian Doane <brianmdoane@gmail.com>
2016-08-12 00:27:19 -0500

    able to query artist from Spotify to match rider lists

--- app/controllers/ArtViewCtrl.js
+++ app/controllers/ArtViewCtrl.js
@@ -3,19 +3,29 @@
 app.controller('ArtViewCtrl', function($scope, RidersFactory, $routeParams, UserFactory, $location, SpotifyFactory) {
 
 	// Routeparams prop can be any namespace, just needs to match app.config
-	// Is this bad to have hanging out??
-
-
+	// Art Manager rider 
 	RidersFactory.getRiderFB($routeParams.riderId)
 	.then(function(riderObj) {
-		console.log("ArtViewRiderObj", riderObj);
+		// console.log("ArtViewRiderObj", riderObj);
 		$scope.currentRider = riderObj;
+		// How can i get this out of here??
+		SpotifyFactory.addArtistInfo($scope.currentRider.name)
+		.then(function(artistDetails) {
+			console.log("artistDetails", artistDetails.artists.items[0].id);
+			$scope.currentArtist = artistDetails;
+		});
 	});
 
+	// Venue rider
 	RidersFactory.getVenueRider($routeParams.riderId)
 	.then(function(riderObj) {
-		console.log("ArtViewRiderObj", riderObj);
+		// console.log("ArtViewRiderObj", riderObj);
 		$scope.currentVenRider = riderObj;
+		SpotifyFactory.addArtistInfo($scope.currentVenRider.name)
+		.then(function(artistDetails) {
+			console.log("artistDetails", artistDetails.artists.items[0].id);
+			$scope.currentArtist = artistDetails;
+		});
 	});
 
 	// To use for venue.  If currentUser is !isAdmin
@@ -26,11 +36,7 @@ app.controller('ArtViewCtrl', function($scope, RidersFactory, $routeParams, User
 		console.log("viewArtCurrentUser", $scope.currentUser);
 	});
 
-	// SpotifyFactory.addArtistInfo(currentRider.name)
-	// .then(function(artistDetails) {
-	// 	console.log("artistDetails", artistDetails);
-	// 	$scope.currentArtist = artistDetails;
-	// })
+
 
 	// Add venueUserId to rider and post to FB (for myArtist venue view)
 	$scope.addVenToRider = function(venueUserId) {
@@ -43,22 +49,3 @@ app.controller('ArtViewCtrl', function($scope, RidersFactory, $routeParams, User
 	};
 
 });
-	// RidersFactory.getRiderFB($routeParams.riderId)
-	// .then(function(riderObj) {
-	// 	console.log("ArtViewRiderObj", riderObj);
-	// 	$scope.currentRider = riderObj;
-	// });
-
-	// RidersFactory.getVenueRider($routeParams.riderId)
-	// .then(function(riderObj) {
-	// 	console.log("ArtViewRiderObj", riderObj);
-	// 	$scope.currentRider = riderObj;
-	// });
-
-
-
-
-
-
-
-
--- app/factories/SpotifyFactory.js
+++ app/factories/SpotifyFactory.js
@@ -9,7 +9,7 @@ app.factory('SpotifyFactory', function($q, $http) {
 			let query = `q=${artist}&type=artist`;
 			$http.get(`https://api.spotify.com/v1/search?${query}`)
 			.success(function(artistObj) {
-				console.log("artisObj", artisObj);
+				console.log("artistObj", artistObj);
 				resolve(artistObj);
 			})
 			.error(function(error) {
@@ -17,6 +17,12 @@ app.factory('SpotifyFactory', function($q, $http) {
 			});
 		});
 	};
+
+	// const getArtistImage = function(id) {
+	// 	return $q(function(resolve, reject) {
+	// 		$http.get(`https://api.spotify.com/v1/search`)
+	// 	});
+	// };
 
 	return { addArtistInfo };
 
