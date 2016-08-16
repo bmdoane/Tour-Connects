7edeb7a88cebe8988e1c9bee3bc9001966aba419
Brian Doane <brianmdoane@gmail.com>
2016-08-15 23:47:30 -0500

    basic styling added for artist search

--- app/controllers/ArtSearchCtrl.js
+++ app/controllers/ArtSearchCtrl.js
@@ -1,6 +1,10 @@
 "use strict";
 
-app.controller('ArtSearchCtrl', function($scope, RidersFactory) {
+app.controller('ArtSearchCtrl', function($scope, RidersFactory, $timeout) {
+
+	$timeout(function() {
+		$('#search').focus(); // Look at ng-focus - get jquery out
+	}, 50);
 
 	RidersFactory.getAllRidersFB()
 	.then(function(riderCollection) {
